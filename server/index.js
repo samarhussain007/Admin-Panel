514869a950a91f05b2b2414fa92e9b1fa402f395
samarhussain007 <hussainsamar36@gmail.com>
2023-03-15 00:45:58 +0530

    First Commit

--- server/index.js
+++ server/index.js
@@ -11,6 +11,10 @@ import genralRoutes from "./routes/general.js";
 import managementRoutes from "./routes/management.js";
 import salesRoutes from "./routes/sales.js";
 
+// DATA IMPORTS
+import User from "./models/User.js";
+import { dataUser } from "./data/index.js";
+
 // CONFIGURATION//
 
 dotenv.config();
@@ -41,6 +45,8 @@ mongoose
     app.listen(PORT, () => {
       console.log(`Server running on port: ${PORT}`);
     });
+    // Only Add one Time
+    // User.insertMany(dataUser);
   })
   .catch((error) => {
     console.log(error.message);
