45593eabd2d46e7e76fe551a054a10fb3ef22c62
samarhussain007 <hussainsamar36@gmail.com>
2023-03-23 11:37:11 +0530

    Added Dashboard

--- client/src/components/OverviewChart.jsx
+++ client/src/components/OverviewChart.jsx
@@ -87,6 +87,7 @@ const OverviewChart = ({ isDashboard = false, view }) => {
       }}
       yFormat=" >-.2f"
       curve="catmullRom"
+      enableArea={isDashboard}
       axisTop={null}
       axisRight={null}
       axisBottom={{
@@ -95,6 +96,7 @@ const OverviewChart = ({ isDashboard = false, view }) => {
           return v;
         },
         orient: "bottom",
+
         tickSize: 5,
         tickPadding: 5,
         tickRotation: 0,
@@ -104,6 +106,7 @@ const OverviewChart = ({ isDashboard = false, view }) => {
       }}
       axisLeft={{
         orient: "left",
+        tickValues: 5,
         tickSize: 5,
         tickPadding: 5,
         tickRotation: 0,
